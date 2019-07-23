b4b8c5353fe90eb8c435c2b5f63396ecc9ae5913
Girish Ramnani <girishramnani95@gmail.com>
2019-07-23 20:27:33 +0530

    added full workflow

--- pkg/handlers/request_manager.go
+++ pkg/handlers/request_manager.go
@@ -1,17 +1,36 @@
 package handlers
 
-import "github.com/moorage/cloud-hugo/pkg/config"
-import "github.com/moorage/cloud-hugo/pkg/utils"
+import (
+	"cloud.google.com/go/pubsub"
+	"github.com/moorage/cloud-hugo/pkg/builder"
+	"github.com/moorage/cloud-hugo/pkg/config"
+	"github.com/moorage/cloud-hugo/pkg/git"
+	"github.com/moorage/cloud-hugo/pkg/utils"
+)
+
+// GitMsg is the message which a publisher sends when a repo needs to be cloned or pulled
+type GitMsg struct {
+	GitURL string `json:"git_url"`
+}
 
 // ReqManager contains all the necessary context and dependencies
 type ReqManager struct {
-	cfg      *config.PublisherConfig
-	RepoName string
+	cfg       *config.PublisherConfig
+	gitClient *git.GitClient
+	topic     *pubsub.Topic
+	builder   builder.Builder
+	RepoName  string
 }
 
-func NewReqManager(cfg *config.PublisherConfig) *ReqManager {
+func NewReqManager(cfg *config.PublisherConfig,
+	gitClient *git.GitClient,
+	topic *pubsub.Topic,
+	builder builder.Builder) *ReqManager {
 	return &ReqManager{
-		cfg:      cfg,
-		RepoName: utils.ExtractNameFromGitURL(cfg.RepoURL),
+		cfg:       cfg,
+		gitClient: gitClient,
+		builder:   builder,
+		topic:     topic,
+		RepoName:  utils.ExtractNameFromGitURL(cfg.RepoURL),
 	}
 }
--- pkg/handlers/save_file.go
+++ pkg/handlers/save_file.go
@@ -1,12 +1,17 @@
 package handlers
 
 import (
+	"context"
+	"encoding/json"
+	"log"
 	"path/filepath"
 
 	"io/ioutil"
 	"net/http"
 
+	"cloud.google.com/go/pubsub"
 	"github.com/labstack/echo"
+	ghttp "gopkg.in/src-d/go-git.v4/plumbing/transport/http"
 )
 
 type SaveFileReq struct {
@@ -25,7 +30,34 @@ func (rqm *ReqManager) SaveFile(c echo.Context) error {
 	if err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
 	}
+
+	err = rqm.pubsubWorflow()
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+
 	return c.JSON(200, map[string]string{
 		"message": "success",
 	})
+}
+
+func (rqm *ReqManager) pubsubWorflow() error {
+	output, err := rqm.builder.Build()
+	if err != nil {
+		return err
+	}
+
+	log.Println(output)
+
+	err = rqm.gitClient.CommitAndPush(rqm.cfg.RepoURL, rqm.cfg.UserName, rqm.cfg.UserEmail,
+		&ghttp.BasicAuth{
+			Username: rqm.cfg.UserName,
+			Password: rqm.cfg.AccessToken,
+		})
+
+	d, err := json.Marshal(GitMsg{
+		GitURL: rqm.cfg.RepoURL,
+	})
+	_, err = rqm.topic.Publish(context.Background(), &pubsub.Message{Data: d})
+	return err
 }
