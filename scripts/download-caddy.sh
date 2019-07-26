4e086fad7fda9f8cec996ed0e5386ec1b4d371fe
Matthew Moore <m3moore@gmail.com>
2019-07-26 16:45:44 -0700

    make caddy dl work on mac

--- scripts/download-caddy.sh
+++ scripts/download-caddy.sh
@@ -1,3 +1,11 @@
-wget -O /tmp/caddy.tar.gz https://caddyserver.com/download/linux/amd64?license=personal
-tar -xzf /tmp/caddy.tar.gz --directory /tmp
-cp /tmp/caddy ./
+DLURL=https://caddyserver.com/download/linux/amd64?license=personal
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  DLURL=https://caddyserver.com/download/darwin/amd64?license=personal
+fi
+mkdir ~/tmp
+mkdir ~/tmp/caddy
+wget -O ~/tmp/caddy/caddy.tar.gz $DLURL
+tar -xzf ~/tmp/caddy/caddy.tar.gz --directory ~/tmp/caddy
+mv ~/tmp/caddy/caddy ./
+rm -rf ~/tmp/caddy
+rmdir ~/tmp
